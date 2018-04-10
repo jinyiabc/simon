dabac6c729cdb447909732d599066a3b1b00d952
Alex J.Y <13817282645@163.com>
2018-04-11 00:09:08 +0800

    update

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'img')));
+app.use(express.static(path.join(__dirname, 'audio')));
+
 
 // app.use('/', index);
 // app.use('/users', users);
