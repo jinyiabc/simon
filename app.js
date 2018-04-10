0b143e20546afc3cac3f924d202e82f4e192a17e
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2018-04-10 17:44:40 +0800

    update

--- app.js
+++ app.js
@@ -20,11 +20,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'img')));
 
-app.use('/', index);
-app.use('/users', users);
-
+// app.use('/', index);
+// app.use('/users', users);
+app.use('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
